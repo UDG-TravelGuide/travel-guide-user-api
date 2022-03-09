4fff10351ce3f00ac65fff5bfd6f83bf1ab1667e
Hamza Saddouki <hsaddouki@gmail.com>
2022-03-09 17:48:45 +0100

    Afegir Model Usuari

---
+++ models/User.ts
@@ -0,0 +1,48 @@
+import { Sequelize, STRING, INTEGER, Model, ModelCtor } from 'sequelize';
+import { getSequelize } from '../config/dbConfig';
+
+const sequelize: Sequelize = getSequelize();
+
+const getUser = () => {
+    let userModel: ModelCtor<Model<any, any>>;
+    if (sequelize) {
+        userModel = sequelize.define('User', {
+            id: {
+                type: INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            userName: {
+                type: STRING,
+                allowNull: false
+            },
+            email: {
+                type: STRING,
+                allowNull: false
+            },
+            password: {
+                type: STRING,
+                allowNull: false
+            },
+            birthDate: {
+                type: STRING,
+                allowNull: true
+            },
+            profilePhoto: {
+                type: STRING,
+                allowNull: true
+            },
+            points: {
+                type: INTEGER,
+                allowNull: false,
+                defaultValue: 0
+            }
+        });
+    } else {
+        userModel = null;
+    }
+    return userModel;
+}
+
+export const User = getUser();
