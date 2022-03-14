021e25b79607aa9233cc30039d574a8fa9c810e1
Hamza Saddouki <hsaddouki@gmail.com>
2022-03-14 18:01:24 +0100

    Afegir URI de base de dades

--- config/dbConfig.ts
+++ config/dbConfig.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize: Sequelize = new Sequelize( process.env.DATABASE_URI, {
+const sequelize: Sequelize = new Sequelize( 'postgres://ynwcapgdzxydic:8d2a170e98176ec1163e55368ab96e8d1c94032f6fbb89fe6401b61819fdcd06@ec2-52-209-185-5.eu-west-1.compute.amazonaws.com:5432/d60798l516ib5g', {
     dialectOptions: {
         ssl: {
             rejectUnauthorized: false
