318977f7138413388ecd1f4bd095530e1dc4d0ac
Hamza Saddouki <hsaddouki@gmail.com>
2022-05-09 14:31:58 +0200

    Modificacions configuració

--- config/dbConfig.ts
+++ config/dbConfig.ts
@@ -1,6 +1,4 @@
 import { Sequelize } from "sequelize";
-import dotenv from 'dotenv';
-dotenv.config();
 
 const sequelize: Sequelize = new Sequelize( process.env.DATABASE_URI , {
     dialectOptions: {
