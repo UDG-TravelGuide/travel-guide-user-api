fec44aa52fccffd846dd33d3e189a399de586508
Hamza Saddouki <hsaddouki@gmail.com>
2022-09-04 13:45:04 +0200

    Provem un altre copp

--- controllers/CountryController.ts
+++ controllers/CountryController.ts
@@ -7,7 +7,7 @@ export const getAllCountries = async( _ = request, res = response ): Promise<voi
     const LOGGER_BASE = `getAllCountries@CountryController -`;
 
     try {
-        get(`https://restcountries.com/v2/all?fields=name,capital,currencies`, (resp) => {
+        get(`https://restcountries.com/v3/all?fields=name,cca2,cca3`, (resp) => {
             let data = '';
 
             resp.on('data', (chunk) => {
